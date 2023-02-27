fcf3e78dbcb3db3f158bdf59913cc8aad7b0fb8a
zaryabdev <zaryabdev404@gmail.com>
2023-02-27 20:16:50 +0500

    fixes

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -6,27 +6,9 @@ export default [
         link: "#",
     },
     {
-        title: "Javascript Calculator",
-        imgUrl: "/assets/js-calculator-app.webp",
-        stack: ["Javascript", "CSS"],
-        link: "#",
-    },
-    {
-        title: "Multi-select React-dnd",
-        imgUrl: "/assets/react-multi-select.png",
-        stack: ["React", "React-dnd", "Vite"],
-        link: "#",
-    },
-    {
-        title: "React Calculator",
-        imgUrl: "/assets/react-calculator-app.webp",
-        stack: ["React", "Javascript", "CSS"],
-        link: "#",
-    },
-    {
-        title: "Travelmart",
-        imgUrl: "/assets/travelmart.png",
-        stack: ["React", "Javascript", "Bootstrap"],
+        title: "E Commerce",
+        imgUrl: "/assets/e-com.png",
+        stack: ["NodeJs", "React", "MongoDB"],
         link: "#",
     },
 ];
